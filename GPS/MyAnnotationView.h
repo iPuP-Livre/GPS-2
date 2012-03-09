cdabf154c4d6075ae19062d6fabba55a448619c2
ipodishima <marian.paul2@gmail.com>
2012-03-08 23:13:41 -0500

    initial import of MyAnnotationView

---
+++ GPS/MyAnnotationView.h
@@ -0,0 +1,13 @@
+//
+//  MyAnnotationView.h
+//  GPS
+//
+//  Created by Marian PAUL on 08/03/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MyAnnotationView : MKAnnotationView
+
+@end
---
+++ GPS/MyAnnotationView.m
@@ -0,0 +1,31 @@
+//
+//  MyAnnotationView.m
+//  GPS
+//
+//  Created by Marian PAUL on 08/03/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import "MyAnnotationView.h"
+
+@implementation MyAnnotationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
