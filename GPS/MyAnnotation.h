2aed4314de10b325d9c57b2a3e8888d72c4aa34d
ipodishima <marian.paul2@gmail.com>
2012-03-08 22:48:20 -0500

    added the my annotation file

---
+++ GPS/MyAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.h
+//  GPS
+//
+//  Created by Marian PAUL on 08/03/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyAnnotation : NSObject
+
+@end
---
+++ GPS/MyAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyAnnotation.m
+//  GPS
+//
+//  Created by Marian PAUL on 08/03/12.
+//  Copyright (c) 2012 iPuP SARL. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+@implementation MyAnnotation
+
+@end
